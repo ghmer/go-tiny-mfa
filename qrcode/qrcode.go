391a318a64a6e7832f0ff888865c75a004b20f52
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-07-09 00:49:37 +0200

    adding comments, fixing FormatString

--- qrcode/qrcode.go
+++ qrcode/qrcode.go
@@ -6,13 +6,14 @@ import (
 	qrcode "github.com/skip2/go-qrcode"
 )
 
-const FormatString string = "otpauth://totp/%1$s:%2$s@%1$s?algorithm=SHA1&digits=6&issuer=%1$s&period=30&secret=%3$s"
+//FormatString is a predefined String that is used when generating a QR Code
+const FormatString string = "otpauth://totp/%s:%s@%s?algorithm=SHA1&digits=6&issuer=%s&period=30&secret=%s"
 
-// Generates a QRCode of the totp url
+// GenerateQrCode Generates a QRCode of the totp url
 func GenerateQrCode(issuer, username, secret string) ([]byte, error) {
 	var png []byte
-	otpauthUrl := fmt.Sprintf(FormatString, issuer, username, issuer, issuer, secret)
-	png, err := qrcode.Encode(otpauthUrl, qrcode.Medium, 256)
+	otpauthURL := fmt.Sprintf(FormatString, issuer, username, issuer, issuer, secret)
+	png, err := qrcode.Encode(otpauthURL, qrcode.Medium, 256)
 
 	return png, err
 }
