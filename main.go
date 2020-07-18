b589ff0aa3474592a1420a37e1bc9fc6d4c45fa1
Mario Enrico Ragucci <mario@parzival.link>
2020-07-18 13:17:07 +0200

    pushing playground

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/base32"
 	"fmt"
 	"go-tiny-mfa/core"
+	"go-tiny-mfa/qrcode"
+	"strings"
+	"time"
 )
 
 func cleanup() {
@@ -11,47 +14,64 @@ func cleanup() {
 }
 func main() {
 	/*
-		var connURL string = os.Args[1]
-		var port string = os.Args[2]
+			var connURL string = os.Args[1]
+			var port string = os.Args[2]
 
-		db := middleware.CreateConnection(connURL)
-		defer middleware.CloseConnection(db)
+			db := middleware.CreateConnection(connURL)
+			defer middleware.CloseConnection(db)
 
-		c := make(chan os.Signal)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		go func() {
-			<-c
-			cleanup()
-			middleware.CloseConnection(db)
-			os.Exit(1)
-		}()
+			c := make(chan os.Signal)
+			signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+			go func() {
+				<-c
+				cleanup()
+				middleware.CloseConnection(db)
+				os.Exit(1)
+			}()
 
-		r := router.Router()
-		// fs := http.FileServer(http.Dir("build"))
-		// http.Handle("/", fs)
-		fmt.Println(fmt.Sprintf("Start serving on port %s", port))
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+			r := router.Router()
+			// fs := http.FileServer(http.Dir("build"))
+			// http.Handle("/", fs)
+			fmt.Println(fmt.Sprintf("Start serving on port %s", port))
+			log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+
+
+		key, err := core.GenerateStandardSecretKey()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		str := base32.StdEncoding.EncodeToString(key)
+		fmt.Println(str)
+
+		key, err = core.GenerateExtendedSecretKey()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		str = base32.StdEncoding.EncodeToString(key)
+		fmt.Println(str)
+
+			16
+			D5WZMKC5I3FDIW7FXG6P2KSVGA======
+			32
+			KWHWKSJJY7TXIDWAUHHAVAV3YULTW3ERZ6ZVUGZ7F46HBMVLTFZA====
 	*/
 
-	key, err := core.GenerateStandardSecretKey()
+	key, err := base32.StdEncoding.DecodeString("D5WZMKC5I3FDIW7FXG6P2KSVGA======")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	str := base32.StdEncoding.EncodeToString(key)
-	fmt.Println(str)
+	fmt.Printf("% x\n", key)
 
-	key, err = core.GenerateExtendedSecretKey()
-	if err != nil {
-		fmt.Println(err)
-	}
+	qrcode.WriteQrCodeImage("issuer.net", "mario", "D5WZMKC5I3FDIW7FXG6P2KSVGA", "/tmp/image.png")
 
-	str = base32.StdEncoding.EncodeToString(key)
-	fmt.Println(str)
-	/*
-		16
-		D5WZMKC5I3FDIW7FXG6P2KSVGA======
-		32
-		KWHWKSJJY7TXIDWAUHHAVAV3YULTW3ERZ6ZVUGZ7F46HBMVLTFZA====
-	*/
+	fmt.Println(core.GenerateValidToken(time.Now().Unix(), key, 0))
+	fmt.Println(core.ValidateTokenCurrentTimestamp(248440, key))
+
+	result := strings.Index("D5WZMKC5I3FDIW7FXG6P2KSVGA======", "=")
+	
+	fmt.Println(result)
+	fmt.Println("D5WZMKC5I3FDIW7FXG6P2KSVGA======"[:result])
 }
