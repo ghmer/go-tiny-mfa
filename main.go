6cdc5652e13506f14a71ba51b02e5014a570562d
Mario Enrico Ragucci <mario@parzival.link>
2020-07-18 11:37:44 +0200

    pushing custom playground

--- main.go
+++ main.go
@@ -1,38 +1,57 @@
 package main
 
 import (
+	"encoding/base32"
 	"fmt"
-	"go-tiny-mfa/middleware"
-	"go-tiny-mfa/router"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
+	"go-tiny-mfa/core"
 )
 
 func cleanup() {
 	fmt.Println("Cleanup called")
 }
 func main() {
-	var connURL string = os.Args[1]
-	var port string = os.Args[2]
+	/*
+		var connURL string = os.Args[1]
+		var port string = os.Args[2]
 
-	db := middleware.CreateConnection(connURL)
-	defer middleware.CloseConnection(db)
+		db := middleware.CreateConnection(connURL)
+		defer middleware.CloseConnection(db)
 
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		cleanup()
-		middleware.CloseConnection(db)
-		os.Exit(1)
-	}()
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-c
+			cleanup()
+			middleware.CloseConnection(db)
+			os.Exit(1)
+		}()
 
-	r := router.Router()
-	// fs := http.FileServer(http.Dir("build"))
-	// http.Handle("/", fs)
-	fmt.Println(fmt.Sprintf("Start serving on port %s", port))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+		r := router.Router()
+		// fs := http.FileServer(http.Dir("build"))
+		// http.Handle("/", fs)
+		fmt.Println(fmt.Sprintf("Start serving on port %s", port))
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+	*/
+
+	key, err := core.GenerateStandardSecretKey()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	str := base32.StdEncoding.EncodeToString(key)
+	fmt.Println(str)
+
+	key, err = core.GenerateExtendedSecretKey()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	str = base32.StdEncoding.EncodeToString(key)
+	fmt.Println(str)
+	/*
+		16
+		D5WZMKC5I3FDIW7FXG6P2KSVGA======
+		32
+		KWHWKSJJY7TXIDWAUHHAVAV3YULTW3ERZ6ZVUGZ7F46HBMVLTFZA====
+	*/
 }
