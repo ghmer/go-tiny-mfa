2ab455236e342e2b71ce7663bf05cd2c189b430e
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-07-23 22:05:02 +0200

    align struct with database model

--- structs/user.go
+++ structs/user.go
@@ -2,7 +2,9 @@ package structs
 
 // User is a struct that holds Username, Issuer and the Base32 encoded Secret Key
 type User struct {
-	Username  string
-	Issuer    string
-	Base32Key []byte
+	ID               string
+	Username         string
+	Issuer           string
+	CryptedBase32Key string
+	enabled          bool
 }
