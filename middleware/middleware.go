35af50e6a67885fedcd6fef2872d1004d57df4ad
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-07-25 00:24:48 +0200

    establish and close on each call

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"go-tiny-mfa/structs"
+	"os"
 
 	"github.com/google/uuid"
 
@@ -12,8 +13,8 @@ import (
 )
 
 // CreateConnection creates a connection to a postgres DB
-func CreateConnection(connectionURL string) *sql.DB {
-	db, err := sql.Open("postgres", connectionURL)
+func CreateConnection() *sql.DB {
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		panic(err)
 	}
@@ -24,18 +25,13 @@ func CreateConnection(connectionURL string) *sql.DB {
 	}
 
 	fmt.Println("Successfully connected!")
-	initializeDatabase(db)
-	// return the connection
+
 	return db
 }
 
-// CloseConnection closes the connection to the db
-func CloseConnection(db *sql.DB) error {
-	fmt.Println("closing connection!")
-	return db.Close()
-}
-
-func initializeUserTable(db *sql.DB) {
+func initializeUserTable() {
+	db := CreateConnection()
+	defer db.Close()
 	createstring := `CREATE TABLE IF NOT EXISTS accounts (
 		id varchar(45) NOT NULL,
 		username varchar(32) NOT NULL,
@@ -52,7 +48,9 @@ func initializeUserTable(db *sql.DB) {
 	}
 }
 
-func initializeIssuerTable(db *sql.DB) {
+func initializeIssuerTable() {
+	db := CreateConnection()
+	defer db.Close()
 	createstring := `CREATE TABLE IF NOT EXISTS issuer (
 		id varchar(45) NOT NULL,
 		name varchar(32) NOT NULL UNIQUE,
@@ -66,13 +64,15 @@ func initializeIssuerTable(db *sql.DB) {
 	}
 }
 
-func initializeDatabase(db *sql.DB) {
-	initializeIssuerTable(db)
-	initializeUserTable(db)
+func initializeDatabase() {
+	initializeIssuerTable()
+	initializeUserTable()
 }
 
 //InsertUser inserts a userstruct to the DB
-func InsertUser(user structs.User, db *sql.DB) {
+func InsertUser(user structs.User) {
+	db := CreateConnection()
+	defer db.Close()
 	sqlInsert := `INSERT INTO accounts (id, username, email, issuer_id, key, enabled)
 				VALUES ($1, $2, $3, $4, $5)
 				RETURNING id`
@@ -94,7 +94,9 @@ func CreateIssuer(issuer, contact string, enabled bool) structs.Issuer {
 }
 
 //InsertIssuer inserts a Issuer struct to the database
-func InsertIssuer(issuer structs.Issuer, db *sql.DB) {
+func InsertIssuer(issuer structs.Issuer) {
+	db := CreateConnection()
+	defer db.Close()
 	sqlInsert := `INSERT INTO issuer (id, name, contact, enabled)
 				VALUES ($1, $2, $3, $4)
 				RETURNING id`
@@ -108,7 +110,9 @@ func InsertIssuer(issuer structs.Issuer, db *sql.DB) {
 }
 
 //DeleteIssuer deletes an issuer from the database
-func DeleteIssuer(issuer string, db *sql.DB) {
+func DeleteIssuer(issuer string) {
+	db := CreateConnection()
+	defer db.Close()
 	sqlDelete := `DELETE FROM issuer WHERE name=$1`
 	res, err := db.Exec(sqlDelete, issuer)
 	if err != nil {
@@ -120,7 +124,9 @@ func DeleteIssuer(issuer string, db *sql.DB) {
 }
 
 //GetIssuer returns the requested issuer from the database as Issuer struct
-func GetIssuer(issuer string, db *sql.DB) (structs.Issuer, error) {
+func GetIssuer(issuer string) (structs.Issuer, error) {
+	db := CreateConnection()
+	defer db.Close()
 	sqlSelect := `SELECT * FROM issuer where name=$1`
 	res, err := db.Query(sqlSelect, issuer)
 	if err != nil {
@@ -137,7 +143,46 @@ func GetIssuer(issuer string, db *sql.DB) (structs.Issuer, error) {
 	return issuerStruct, nil
 }
 
+//GetIssuers returns all Issuers from the database
+func GetIssuers() ([]structs.Issuer, error) {
+	db := CreateConnection()
+	defer db.Close()
+	sqlCountSelect := `SELECT COUNT(id, name, contact, enabled) FROM issuer`
+	res, err := db.Query(sqlCountSelect)
+	if err != nil {
+		return []structs.Issuer{}, err
+	}
+
+	var count int
+	res.Scan(&count)
+
+	issuers := make([]structs.Issuer, count)
+
+	sqlSelect := `SELECT id, name, contact, enabled FROM issuer`
+	rows, errorMessage := db.Query(sqlSelect)
+	if errorMessage != nil {
+		return issuers, errorMessage
+	}
+
+	loop := 0
+	for rows.Next() {
+		var id string
+		var name string
+		var contact string
+		var enabled bool
+		rows.Scan(&id, &name, &contact, &enabled)
+
+		issuerStruct := structs.Issuer{ID: id, Name: name, Contact: contact, Enabled: enabled}
+		issuers[loop] = issuerStruct
+		loop++
+	}
+
+	rows.Close()
+
+	return issuers, nil
+}
+
 //GetUser returns a User struct based on given username and issuer
-func GetUser(username, issuer string, db *sql.DB) {
+func GetUser(username, issuer string) {
 
 }
