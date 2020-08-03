5621f9ac7c090bf3cbb0eab33c7dc79050a750af
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-08-03 22:19:52 +0200

    generic token

--- structs/accesstoken.go
+++ structs/accesstoken.go
@@ -1,6 +1,8 @@
 package structs
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 )
 
@@ -18,46 +20,26 @@ const (
 //Token is a struct that can be used to define access to a resource
 type Token struct {
 	ObjectRefID string `json:"-"`
-	ActionRefID int    `json:"-"`
 	Token       string `json:"access-token"`
+	Description string `json:"description"`
 }
 
-//NewFullAccessToken generates a new access token that has full access on the given object
-func NewFullAccessToken(objectid string) Token {
+//NewAccessToken generates a new access token that has full access on the given object
+func NewAccessToken(args ...string) Token {
 	var token Token
-	token.ObjectRefID = objectid
-	token.ActionRefID = ActionIDFull
 	token.Token = uuid.New().String()
 
-	return token
-}
-
-//NewGetAccessToken generates a new access token that has GET access on the given object
-func NewGetAccessToken(objectid string) Token {
-	var token Token
-	token.ObjectRefID = objectid
-	token.ActionRefID = ActionIDGet
-	token.Token = uuid.New().String()
-
-	return token
-}
-
-//NewPostAccessToken generates a new access token that has POST access on the given object
-func NewPostAccessToken(objectid string) Token {
-	var token Token
-	token.ObjectRefID = objectid
-	token.ActionRefID = ActionIDPost
-	token.Token = uuid.New().String()
-
-	return token
-}
-
-//NewDeleteAccessToken generates a new access token that has DELETE access on the given object
-func NewDeleteAccessToken(objectid string) Token {
-	var token Token
-	token.ObjectRefID = objectid
-	token.ActionRefID = ActionIDDelete
-	token.Token = uuid.New().String()
+	for i, arg := range args {
+		switch i {
+		case 0:
+			token.ObjectRefID = arg
+			token.Description = "main access token"
+		case 1:
+			token.Description = arg
+		default:
+			fmt.Println(fmt.Sprintf("cannot use argument %s with parameter %d on a new token", arg, i))
+		}
+	}
 
 	return token
 }
