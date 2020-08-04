3ec39d00ab4d33f0cad6fa8d1745467242754c87
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-08-04 21:01:18 +0200

    adding comments explaining the build steps

--- build.sh
+++ build.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
+#build for linux/amd64
 env GOOS=linux GOARCH=amd64 go build -o build/go-tiny-mfa-amd64
+#build for linux/arm64
 env GOOS=linux GOARCH=arm64 GOARM=7 go build -o build/go-tiny-mfa-arm64
+#build for linux/arm
 env GOOS=linux GOARCH=arm GOARM=6 go build -o build/go-tiny-mfa-arm
 
+#purge old manifest
 docker manifest push --purge registry.parzival.link/go-tiny-mfa
+#remove all containers
 docker system prune --volumes --all
 
-#docker build -t registry.parzival.link/go-tiny-mfa:amd64 -f Dockerfile_amd64 .
-#docker build -t registry.parzival.link/go-tiny-mfa:arm64 -f Dockerfile_arm64 .
-#docker build -t registry.parzival.link/go-tiny-mfa:arm -f Dockerfile_arm .
-
+#create docker image for linux/amd64
 docker buildx build --load -t registry.parzival.link/go-tiny-mfa:amd64 --platform=linux/amd64 -f Dockerfile_amd64 .
+#create docker image for linux/arm64
 docker buildx build --load -t registry.parzival.link/go-tiny-mfa:arm64 --platform=linux/arm64 -f Dockerfile_arm64 .
+#create docker image for linux/arm
 docker buildx build --load -t registry.parzival.link/go-tiny-mfa:arm --platform=linux/arm/v7 -f Dockerfile_arm .
 
+#push images to registry
 docker push registry.parzival.link/go-tiny-mfa:arm
 docker push registry.parzival.link/go-tiny-mfa:arm64
 docker push registry.parzival.link/go-tiny-mfa:amd64
 
+#create new :latest manifest 
 docker manifest create \
             registry.parzival.link/go-tiny-mfa \
             registry.parzival.link/go-tiny-mfa:amd64 \
             registry.parzival.link/go-tiny-mfa:arm64 \
             registry.parzival.link/go-tiny-mfa:arm
 
+#push manifest to registry
 docker manifest push registry.parzival.link/go-tiny-mfa
