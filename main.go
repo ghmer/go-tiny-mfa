c8f8fd1df42bfc41496fcfeb86b1b8570eabc59e
Mario Enrico Ragucci <mario@whisperedshouts.de>
2021-03-28 04:02:29 +0200

    extend healthcheck function

--- main.go
+++ main.go
@@ -16,15 +16,17 @@ import (
 // Healthcheck defines whether we run the router or a simple healthcheck
 var DoHealthcheck bool = false
 
-// HealthchechUrl is the url we are using to retrieve the status
-var HealthcheckUrl string = "http://127.0.0.1:57687/api/v1/health"
-
 func init() {
+	// check whether we shall act as a healthchecker
 	if len(os.Args) == 2 && os.Args[1] == "--healthcheck" {
 		// we are a healthcheck call, not initializing db
 		DoHealthcheck = true
-	} else {
+	}
+
+	// initializing system
+	if !DoHealthcheck {
 		log.Println("initializing")
+
 		// Check if needed environment variables have been set
 		err := checkEnvironmentVariables()
 		if err != nil {
@@ -36,9 +38,9 @@ func init() {
 		if err != nil {
 			log.Fatal(err)
 		}
-	}
 
-	log.Println("initialization finished")
+		log.Println("initialization finished")
+	}
 }
 
 func main() {
@@ -46,17 +48,17 @@ func main() {
 	if DoHealthcheck {
 		returncode := Healthcheck()
 		os.Exit(returncode)
-	} else {
-		// Create the router
-		r := router.Router()
-		config, err := middleware.GetSystemConfiguration()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.Println("Start serving on port", config.RouterPort)
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.RouterPort), r))
 	}
+
+	// Create the router
+	r := router.Router()
+	config, err := middleware.GetSystemConfiguration()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Start serving on port", config.RouterPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.RouterPort), r))
 }
 
 // check whether all variables needed for a proper system startup are set
@@ -95,7 +97,16 @@ func Healthcheck() int {
 		Timeout: time.Duration(5 * time.Second),
 	}
 
-	result, err := client.Get(HealthcheckUrl)
+	routerport, err := middleware.GetSystemProperty(middleware.RouterPortKey)
+	if err != nil {
+		returncode = 1
+		return returncode
+	}
+
+	// healthchechUrl is the url we are using to retrieve the status
+	var healthcheckUrl string = fmt.Sprintf("http://127.0.0.1:%s/api/v1/health", routerport)
+
+	result, err := client.Get(healthcheckUrl)
 	if err != nil {
 		returncode = 1
 		return returncode
