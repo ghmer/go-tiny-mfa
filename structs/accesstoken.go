55d41f4195ee8c39ce9bc2d4dc239f2c47e72d25
Mario Enrico Ragucci <mario@parzival.link>
2021-04-01 00:03:01 +0200

    Add struct TokenEntry
    Signed-off-by: Mario Enrico Ragucci <mario@parzival.link>

--- structs/accesstoken.go
+++ structs/accesstoken.go
@@ -9,10 +9,18 @@ import (
 
 //Token is a struct that can be used to define access to a resource
 type Token struct {
-	ID          string `json:"id"`
+	ID          string `json:"-"`
 	ObjectRefID string `json:"-"`
 	Token       string `json:"access-token"`
 	Description string `json:"description"`
+}
+
+//TokenEntry is used when returning registered tokens
+type TokenEntry struct {
+	Id             string `json:"id"`
+	Description    string `json:"description"`
+	CreatedOn      string `json:"created_on"`
+	LastAccessTime string `json:"last_access_time"`
 }
 
 //NewAccessToken generates a new access token that has full access on the given object
