a0e715ecc57ec18b0ac46e5ff43d17e1f00cb113
Mario Enrico Ragucci <mario@parzival.link>
2021-04-01 00:03:55 +0200

    add production switch
    Signed-off-by: Mario Enrico Ragucci <mario@parzival.link>

--- build.sh
+++ build.sh
@@ -39,47 +39,88 @@ env GOOS=linux GOARCH=arm64 GOARM=7 go build -ldflags "-s -w" -o build/go-tiny-m
 #build for linux/arm
 env GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-s -w" -o build/go-tiny-mfa-arm
 
-#purge old manifest
-${DOCKER} manifest push --purge ${REPOSITORY}/go-tiny-mfa
-${DOCKER} manifest push --purge ${REPOSITORY}/go-tiny-mfa:${VERSION}
-#remove all containers
-${DOCKER} system prune --volumes --all -f
+#build for production?
+if [[ ${PRODUCTION} == "true" ]]; then 
+    echo "Build for Production"; 
+    #purge old manifest
+    ${DOCKER} manifest push --purge ${REPOSITORY}/go-tiny-mfa
+    ${DOCKER} manifest push --purge ${REPOSITORY}/go-tiny-mfa:${VERSION}
+    #remove all containers
+    ${DOCKER} system prune --volumes --all -f
 
-#create docker image for linux/amd64
-${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:amd64 \
-    --platform=linux/amd64  --build-arg ARCH=amd64 \
-    --label version=${VERSION} --label commit=${COMMIT} \
-    --label revision=${REVISION} --file Dockerfile .
-#create docker image for linux/arm64
-${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:arm64 \
-    --platform=linux/arm64  --build-arg ARCH=arm64 \
-    --label version=${VERSION} --label commit=${COMMIT} \
-    --label revision=${REVISION} --file Dockerfile .
-#create docker image for linux/arm
-${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:arm   \
-    --platform=linux/arm/v6 --build-arg ARCH=arm   \
-    --label version=${VERSION} --label commit=${COMMIT} \
-    --label revision=${REVISION} --file Dockerfile .
+    #create docker image for linux/amd64
+    ${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:amd64 \
+        --platform=linux/amd64  --build-arg ARCH=amd64 \
+        --label version=${VERSION} --label commit=${COMMIT} \
+        --label revision=${REVISION} --file Dockerfile .
+    #create docker image for linux/arm64
+    ${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:arm64 \
+        --platform=linux/arm64  --build-arg ARCH=arm64 \
+        --label version=${VERSION} --label commit=${COMMIT} \
+        --label revision=${REVISION} --file Dockerfile .
+    #create docker image for linux/arm
+    ${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:arm   \
+        --platform=linux/arm/v6 --build-arg ARCH=arm   \
+        --label version=${VERSION} --label commit=${COMMIT} \
+        --label revision=${REVISION} --file Dockerfile .
 
-#push images to registry
-${DOCKER} push ${REPOSITORY}/go-tiny-mfa:arm
-${DOCKER} push ${REPOSITORY}/go-tiny-mfa:arm64
-${DOCKER} push ${REPOSITORY}/go-tiny-mfa:amd64
+    #push images to registry
+    ${DOCKER} push ${REPOSITORY}/go-tiny-mfa:arm
+    ${DOCKER} push ${REPOSITORY}/go-tiny-mfa:arm64
+    ${DOCKER} push ${REPOSITORY}/go-tiny-mfa:amd64
 
-#create new :latest manifest 
-${DOCKER} manifest create \
-            ${REPOSITORY}/go-tiny-mfa \
-            ${REPOSITORY}/go-tiny-mfa:amd64 \
-            ${REPOSITORY}/go-tiny-mfa:arm64 \
-            ${REPOSITORY}/go-tiny-mfa:arm
+    #create new :latest manifest 
+    ${DOCKER} manifest create \
+                ${REPOSITORY}/go-tiny-mfa \
+                ${REPOSITORY}/go-tiny-mfa:amd64 \
+                ${REPOSITORY}/go-tiny-mfa:arm64 \
+                ${REPOSITORY}/go-tiny-mfa:arm
 
-#create new :VERSION manifest 
-${DOCKER} manifest create \
-            ${REPOSITORY}/go-tiny-mfa:${VERSION} \
-            ${REPOSITORY}/go-tiny-mfa:amd64 \
-            ${REPOSITORY}/go-tiny-mfa:arm64 \
-            ${REPOSITORY}/go-tiny-mfa:arm
+    #create new :VERSION manifest 
+    ${DOCKER} manifest create \
+                ${REPOSITORY}/go-tiny-mfa:${VERSION} \
+                ${REPOSITORY}/go-tiny-mfa:amd64 \
+                ${REPOSITORY}/go-tiny-mfa:arm64 \
+                ${REPOSITORY}/go-tiny-mfa:arm
 
-#push manifest to registry
-${DOCKER} manifest push ${REPOSITORY}/go-tiny-mfa
-${DOCKER} manifest push ${REPOSITORY}/go-tiny-mfa:${VERSION}
+    #push manifest to registry
+    ${DOCKER} manifest push ${REPOSITORY}/go-tiny-mfa
+    ${DOCKER} manifest push ${REPOSITORY}/go-tiny-mfa:${VERSION}
+else 
+    echo "Build Dev";
+    #purge old manifest
+    ${DOCKER} manifest push --purge ${REPOSITORY}/go-tiny-mfa:development
+    #remove all containers
+    ${DOCKER} system prune --volumes --all -f
+
+    #create docker image for linux/amd64
+    ${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:dev-amd64 \
+        --platform=linux/amd64  --build-arg ARCH=amd64 \
+        --label version=${VERSION} --label commit=${COMMIT} \
+        --label revision=${REVISION} --file Dockerfile .
+    #create docker image for linux/arm64
+    ${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:dev-arm64 \
+        --platform=linux/arm64  --build-arg ARCH=arm64 \
+        --label version=${VERSION} --label commit=${COMMIT} \
+        --label revision=${REVISION} --file Dockerfile .
+    #create docker image for linux/arm
+    ${DOCKER} buildx build --load --tag ${REPOSITORY}/go-tiny-mfa:dev-arm   \
+        --platform=linux/arm/v6 --build-arg ARCH=arm   \
+        --label version=${VERSION} --label commit=${COMMIT} \
+        --label revision=${REVISION} --file Dockerfile .
+
+    #push images to registry
+    ${DOCKER} push ${REPOSITORY}/go-tiny-mfa:dev-arm
+    ${DOCKER} push ${REPOSITORY}/go-tiny-mfa:dev-arm64
+    ${DOCKER} push ${REPOSITORY}/go-tiny-mfa:dev-amd64
+
+    #create new :latest manifest 
+    ${DOCKER} manifest create \
+                ${REPOSITORY}/go-tiny-mfa:development \
+                ${REPOSITORY}/go-tiny-mfa:dev-amd64 \
+                ${REPOSITORY}/go-tiny-mfa:dev-arm64 \
+                ${REPOSITORY}/go-tiny-mfa:dev-arm
+
+    #push manifest to registry
+    ${DOCKER} manifest push ${REPOSITORY}/go-tiny-mfa:development
+fi
